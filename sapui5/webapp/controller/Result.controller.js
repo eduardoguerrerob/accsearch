99d80adf5b9e6097c17d4e2a5936a1cc4a755bd4
eduardoguerrerob <eduardoguerrerob@gmail.com>
2022-05-18 14:44:16 +0000

    20220518: Result smart table

--- sapui5/webapp/controller/Result.controller.js
+++ sapui5/webapp/controller/Result.controller.js
@@ -2,19 +2,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
-    "sap/ui/core/UIComponent"
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel"
 ],
 /**
  * @param {typeof sap.ui.core.mvc.Controller} Controller
  * @param {typeof sap.ui.core.routing.History} History
  * @param {typeof sap.ui.core.UIComponent} UIComponent
+ * @param {typeof sap.ui.model.json.JSONModel} JSONModel
  */
-function (Controller, History, UIComponent) {
+function (Controller, History, UIComponent, JSONModel) {
     "use strict";
+
+    var resultModel = new JSONModel();
 
     return Controller.extend("egb.sapui5.controller.Result", {
         onInit: function () {
-
+            resultModel.loadData("./localService/mockdata/accountsResult.json", false);
+            this.getView().setModel(resultModel, "resultModel");
         },
 
         onNavBack: function() {
